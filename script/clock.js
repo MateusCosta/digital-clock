e1908e64d4e6728210d0252aa01f7c1940a80389
Mateus Costa <contato@mateuscosta.dev>
2024-04-13 22:45:03 -0300

    feat: initial structure of a digital clock

---
+++ script/clock.js
@@ -0,0 +1,36 @@
+var timeEl = document.getElementById('time');
+
+function formatDigitalClock(time){
+    return `<span class="pink">${time.hours.toString().padStart(2, '0')}</span><span class="blue">:</span><span class="orange">${time.minutes.toString().padStart(2, '0')}</span><span class="green">:</span><span class="yellow">${time.seconds.toString().padStart(2, '0')}</span>`
+}
+
+function getCurrentTime(){
+    var currentDate = new Date();
+
+    var time = {
+        hours: currentDate.getHours(),
+        minutes: currentDate.getMinutes(),
+        seconds: currentDate.getSeconds(),
+    };
+
+    return formatDigitalClock(time);
+}
+
+var time = getCurrentTime();
+timeEl.innerHTML = time;
+
+setInterval(function (){
+    var time = getCurrentTime();
+    timeEl.innerHTML = time;
+}, 1000);
+
+function resizeFont(){
+    var containerWidth = document.querySelector('.clock').offsetWidth;
+    var fontSize = containerWidth * 0.2; // Adjust the multiplier as needed
+    document.querySelector('#time').style.fontSize = fontSize + 'px';
+}
+
+resizeFont()
+window.addEventListener('resize', function() {
+    resizeFont();
+  });
